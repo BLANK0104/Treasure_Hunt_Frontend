9dfbfffdbd1518dae0f88fb8a14652dcb7962eab
Utsav <utsavc317@gmail.com>
2025-02-18 15:42:13 +0530

    app.jsx case changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import Footer from './components/footer/Footer';
 import Header from './components/header/Header';
 import Login from './pages/login/Login';
@@ -12,9 +12,11 @@ const App = () => (
     <div>
       <Header />
       <Routes>
-        <Route path="/" element={<Login />} />
+        <Route path="/" element={<Navigate to="/login" replace />} />
+        <Route path="/login" element={<Login />} />
         <Route path="/admin" element={<AdminPage />} />
         <Route path="/participant" element={<ParticipantPage />} />
+        <Route path="*" element={<Navigate to="/login" replace />} />
       </Routes>
       <Footer />
     </div>
